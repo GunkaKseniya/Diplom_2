63f0bdc848d748d0cf37d01f0b56ebf94e8b998a
Gunka Kseniya <gunyasha1990@yandex.ru>
2025-08-28 21:41:45 +0300

    add allure report 2

--- src/main/java/order/OrderGenerate.java
+++ src/main/java/order/OrderGenerate.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 import java.util.List;
 
-public class OrderGenerator {
+public class OrderGenerate {
 
     public Order genericWithValidateIngredients() {
         return new Order(List.of("61c0c5a71d1f82001bdaaa6d", "61c0c5a71d1f82001bdaaa6f"));
